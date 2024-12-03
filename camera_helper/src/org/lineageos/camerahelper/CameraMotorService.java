970bb7f923133e0ee2d947fa72b4d257b291d09b
benjaminbeaudry <bobeaudry@gmail.com>
2024-12-02 20:53:31 -0800

    camera helper: update from hotdog

--- camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
@@ -1,17 +1,7 @@
 /*
  * Copyright (C) 2019 The LineageOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.camerahelper;
@@ -45,8 +35,8 @@ public class CameraMotorService extends Service implements Handler.Callback {
     private CameraManager.AvailabilityCallback mAvailabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
-                public void onCameraClosed(@NonNull String cameraId) {
-                    super.onCameraClosed(cameraId);
+                public void onCameraAvailable(@NonNull String cameraId) {
+                    super.onCameraAvailable(cameraId);
 
                     if (cameraId.equals(FRONT_CAMERA_ID)) {
                         mClosedEvent = SystemClock.elapsedRealtime();
@@ -60,8 +50,8 @@ public class CameraMotorService extends Service implements Handler.Callback {
                 }
 
                 @Override
-                public void onCameraOpened(@NonNull String cameraId, @NonNull String packageId) {
-                    super.onCameraClosed(cameraId);
+                public void onCameraUnavailable(@NonNull String cameraId) {
+                    super.onCameraUnavailable(cameraId);
 
                     if (cameraId.equals(FRONT_CAMERA_ID)) {
                         mOpenEvent = SystemClock.elapsedRealtime();
