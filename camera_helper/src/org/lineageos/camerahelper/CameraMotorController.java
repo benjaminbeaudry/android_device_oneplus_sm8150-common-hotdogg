5110a8f6896094668c8e07e109250a45c23e108a
benjaminbeaudry <bobeaudry@gmail.com>
2024-12-04 18:48:43 -0800

    camera helper: revert to hotdogg

--- camera_helper/src/org/lineageos/camerahelper/CameraMotorController.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorController.java
@@ -1,7 +1,17 @@
 /*
  * Copyright (c) 2019 The LineageOS Project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.camerahelper;
@@ -18,13 +28,13 @@ public class CameraMotorController {
 
     // Camera motor paths
     private static final String CAMERA_MOTOR_ENABLE_PATH =
-            "/sys/class/motor/enable";
+            "/sys/devices/platform/vendor/vendor:motor_pl/enable";
     public static final String CAMERA_MOTOR_HALL_CALIBRATION =
-            "/sys/class/motor/hall_calibration";
+            "/sys/devices/platform/vendor/vendor:motor_pl/hall_calibration";
     private static final String CAMERA_MOTOR_DIRECTION_PATH =
-            "/sys/class/motor/direction";
+            "/sys/devices/platform/vendor/vendor:motor_pl/direction";
     private static final String CAMERA_MOTOR_POSITION_PATH =
-            "/sys/class/motor/position";
+            "/sys/devices/platform/vendor/vendor:motor_pl/position";
 
     // Motor calibration data path
     public static final String CAMERA_PERSIST_HALL_CALIBRATION =
--- camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
@@ -1,7 +1,17 @@
 /*
  * Copyright (C) 2019 The LineageOS Project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.lineageos.camerahelper;
@@ -35,8 +45,8 @@ public class CameraMotorService extends Service implements Handler.Callback {
     private CameraManager.AvailabilityCallback mAvailabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
-                public void onCameraAvailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                public void onCameraClosed(@NonNull String cameraId) {
+                    super.onCameraClosed(cameraId);
 
                     if (cameraId.equals(FRONT_CAMERA_ID)) {
                         mClosedEvent = SystemClock.elapsedRealtime();
@@ -50,8 +60,8 @@ public class CameraMotorService extends Service implements Handler.Callback {
                 }
 
                 @Override
-                public void onCameraUnavailable(@NonNull String cameraId) {
-                    super.onCameraUnavailable(cameraId);
+                public void onCameraOpened(@NonNull String cameraId, @NonNull String packageId) {
+                    super.onCameraClosed(cameraId);
 
                     if (cameraId.equals(FRONT_CAMERA_ID)) {
                         mOpenEvent = SystemClock.elapsedRealtime();
